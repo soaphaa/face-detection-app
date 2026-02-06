7d67f7a6c018d559ba638074cff096ceb7de47eb
soaphaa <sophia.a.luo@gmail.com>
2026-02-06 17:54:48 -0500

    working

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module org.example.facedetectionapp {
     requires javafx.fxml;
 
     requires com.dlsc.formsfx;
+    requires opencv;
 
     opens org.example.facedetectionapp to javafx.fxml;
     exports org.example.facedetectionapp;
--- src/main/java/org/example/facedetectionapp/HelloApplication.java
+++ src/main/java/org/example/facedetectionapp/HelloApplication.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.opencv.core.Core;
 
 import java.io.IOException;
 
@@ -19,6 +20,8 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        System.out.println(Core.VERSION);
         launch();
     }
 }
